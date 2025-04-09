5966d53332ac7b087e8c362b53de61ea91e64eba
creation <creation2224@gmail.com>
2025-04-09 15:28:03 +0530

    project setup done

---
+++ NoFapTracker/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-2xl bg-red-400'>Testing setup</div>
+    </>
+  )
+}
+
+export default App
